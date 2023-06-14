4681dc4596620ebe69ccf0e2de2d5dcff70829b3
Timilehin Jegede <timilehintimothy.jegede@gmail.com>
2023-06-14 18:32:37 +0100

    Initial Commit

---
+++ ui-playground/ui_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_playgroundApp.swift
+//  ui-playground
+//
+//  Created by Timilehin Jegede on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ui_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
